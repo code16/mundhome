06b689e5c801a5fa33f45fa643a8b8fc5ce580d1
Philippe Lonchampt <philippelonchampt@gmail.com>
2019-09-06 16:26:01 +0200

    favicon

--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -7,6 +7,11 @@
         <title>@yield("title") Le Mundhome, Chambres d’hôtes à Mundolsheim</title>
         <meta name="description" content="Chambres d’hôtes de confort à Mundolsheim, Eurométropole de Strasbourg (Alsace / Grand Est, France)">
         <link rel="stylesheet" href="{{ mix('css/main.css', 'assets/build') }}">
+
+        <link rel="apple-touch-icon" sizes="180x180" href="/assets/images/apple-touch-icon.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="/assets/images/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="/assets/images/favicon-16x16.png">
+        <link rel="manifest" href="/assets/site.webmanifest">
     </head>
     <body>
         <div class="container mt-4" id="app">
