f0f57545779801cc0ef19fa4800e9a082d10f587
Philippe Lonchampt <philippelonchampt@gmail.com>
2019-09-06 11:09:33 +0200

    Home page

--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -7,6 +7,68 @@
         <link rel="stylesheet" href="{{ mix('css/main.css', 'assets/build') }}">
     </head>
     <body>
-        @yield('body')
+    @extends('_layouts.master')
+
+        <div class="container mt-2">
+            <div class="header text-center">
+                <div class="logo h1">le Mundhome</div>
+                <div class="baseline mt-2 mb-2">
+                    <div class="font-weight-bold">Chambres d'hôtes</div>
+                    <div><span class="font-weight-bold">de confort</span> à Mundolsheim</div>
+                    <div>Eurométropole Strasbourg</div>
+                </div>
+            </div>
+
+            <div class="menu">
+                <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                        <ul class="navbar-nav m-auto">
+                            <li class="nav-item">
+                                <a class="nav-link" href="/maison">la maison</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="/chambre-1">la chambre 1</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">la chambre 2</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">petit-déjeuner</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">situation</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">carte de fidélité</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">nous contacter</a>
+                            </li>
+                        </ul>
+                    </div>
+                </nav>
+            </div>
+
+            @yield('body')
+
+        </div>
+
+        <footer class="pt-5 pb-5">
+            <div class="container">
+                <div>
+                    <p class="font-weight-bold mt-4 mb-3">Contact</p>
+                    <p>Michel Lonchampt<br>06 60 10 96 24<br><a href="mailto:info@lemundhome.fr">info@lemundhome.fr</a></p>
+                </div>
+                <div>
+                    <p class="font-weight-bold mt-4 mb-3">Accès</p>
+                    <p>14 rue du général de Gaulle<br>67450 Mundolsheim<br><a href="https://goo.gl/maps/5ho8BiQZVrb3YTjP7" target="_blank">Afficher sur Google Maps</a></p>
+                </div>
+            </div>
+        </footer>
+
+        <script src="{{ mix('js/main.js', 'assets/build') }}"></script>
     </body>
 </html>
--- source/index.blade.php
+++ source/index.blade.php
@@ -1,5 +1,69 @@
 @extends('_layouts.master')
 
 @section('body')
-<h1>Hello world!</h1>
+    <img src="/assets/images/exterior-main.jpg" alt="Le Mundhome" class="img-fluid">
+
+    <div class="content">
+        <h1 class="page-title">Bienvenue</h1>
+
+        <p>
+            Le Mundhome vous accueille à Mundolsheim, prononcer [mundols(h)aim] commune de la couronne de Strasbourg.
+            Proche du centre-ville (8 km), Mundolsheim, 5000 habitants environ, fait partie de l’EMS (Eurométropole de Strasbourg).
+        </p>
+        <p>
+            De son ancienne vocation agricole, la commune a conservé de grandes fermes de style alsacien, regroupées dans le vieux village.
+        </p>
+
+        <h2 class="page-title">C’est là que vous pourrez séjourner&nbsp;:</h2>
+    </div>
+
+    <div class="ml-lg-5 pl-lg-5 pr-lg-5 mr-lg-5 mb-5">
+        <div class="row m-auto">
+            <div class="col-6 col-md-3 px-1 py-1">
+                <img src="/assets/images/home/room1.jpg" alt="La chambre 1" class="img-fluid">
+            </div>
+            <div class="col-6 col-md-3 px-1 py-1">
+                <img src="/assets/images/home/room2.jpg" alt="La chambre 2" class="img-fluid">
+            </div>
+            <div class="col-6 col-md-3 px-1 py-1">
+                <img src="/assets/images/home/room3.jpg" alt="La chambre 1 : salle de bains" class="img-fluid">
+            </div>
+            <div class="col-6 col-md-3 px-1 py-1">
+                <img src="/assets/images/home/room4.jpg" alt="La chambre 1" class="img-fluid">
+            </div>
+        </div>
+    </div>
+
+    <div class="content">
+        <p>
+            Que vous voyagiez pour le travail, pour le tourisme, en famille, en amoureux,
+            ou tout simplement pour faire étape,
+            la maison d’hôtes Le Mundhome vous apporte confort, distinction, et sens pratique.
+        </p>
+
+        <ul class="list-unstyled">
+            <li>Les <strong>chambres à grands espaces</strong>, avec bureau et tables de travail, une literie de grande qualité.</li>
+            <li>Les <strong>sanitaires privatifs</strong>, avec grande douche à l’italienne et WC séparés.</li>
+            <li>Le <strong>petit-déjeuner</strong> king-size, pris dans le grand séjour surplombant le jardin.</li>
+            <li>L’<strong>entrée indépendante</strong> des chambres, un propriétaire discret mais attentif à vos demandes.</li>
+            <li>Le charme d’une <strong>maison ancienne</strong>, ferme alsacienne à colombages, dans un environnement de village proche de la grande ville.</li>
+            <li>Les <strong>transports en commun</strong> à proximité immédiate (*).</li>
+        </ul>
+
+        <ul class="list-unstyled">
+            <li>les séjours d’<strong>une seule nuit</strong> sont acceptés,</li>
+            <li>la réservation se fait <strong>sans versement d’acompte</strong>, en confiance,</li>
+            <li>le <strong>paiement par carte</strong> est accepté,</li>
+            <li>la <a href="" class="font-weight-bold">carte de fidélité</a> vous fait bénéficier d’une réduction de 20%.</li>
+        </ul>
+
+        <div class="small mt-5">
+            <p>
+                (*) L’arrêt de bus est à 50 mètres, le bus 73 ou 75 rejoint le centre-ville
+                en 20 minutes (un bus toutes les 30 mn). La gare Sncf est à 15 minutes à pied,
+                le TER rejoint la gare de Strasbourg en 7 minutes (24 trains par jour, soit un
+                train toutes les demi-heures puis toutes les heures).
+            </p>
+        </div>
+    </div>
 @endsection
