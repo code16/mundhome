274bd3f60bb20596dd36d66c4216dbd655d0e0f0
Philippe Lonchampt <philippelonchampt@gmail.com>
2019-09-06 13:23:55 +0200

    Fidelity

--- source/fidelite.blade.php
+++ source/fidelite.blade.php
@@ -15,29 +15,4 @@
             Au Mundhome, il n’y a pas de minimum&nbsp;: nous acceptons les séjours d’une seule nuit.
         </p>
     </div>
-
-    <div class="ml-lg-5 pl-lg-5 pr-lg-5 mr-lg-5 mb-5">
-        <div class="row m-auto">
-            <div class="col-6 col-md-3 px-1 py-1">
-                <a href="/assets/images/breakfast/1.jpg">
-                    <img src="/assets/images/breakfast/1-thumb.jpg" alt="La salle de petit-déjeuner" class="img-fluid">
-                </a>
-            </div>
-            <div class="col-6 col-md-3 px-1 py-1">
-                <a href="/assets/images/breakfast/2.jpg">
-                    <img src="/assets/images/breakfast/2-thumb.jpg" alt="La salle de petit-déjeuner" class="img-fluid">
-                </a>
-            </div>
-            <div class="col-6 col-md-3 px-1 py-1">
-                <a href="/assets/images/breakfast/3.jpg">
-                    <img src="/assets/images/breakfast/3-thumb.jpg" alt="La salle de petit-déjeuner" class="img-fluid">
-                </a>
-            </div>
-            <div class="col-6 col-md-3 px-1 py-1">
-                <a href="/assets/images/breakfast/4.jpg">
-                    <img src="/assets/images/breakfast/4-thumb.jpg" alt="La salle de petit-déjeuner : balcon" class="img-fluid">
-                </a>
-            </div>
-        </div>
-    </div>
 @endsection
